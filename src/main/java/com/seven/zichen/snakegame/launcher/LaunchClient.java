6a7a8902b7a6d3353564836eeebc5dd742ec387f
Zichen Yang <easonyzc96@gmail.com>
2022-12-17 12:03:44 -0500

    -  add udp

---
+++ src/main/java/com/seven/zichen/snakegame/launcher/LaunchClient.java
@@ -0,0 +1,10 @@
+package com.seven.zichen.snakegame.launcher;
+
+public class LaunchClient {
+	public static void main(String[] args) {
+		try {
+			new client.Client();
+		} catch (Exception e) {
+		}
+	}
+}
---
+++ src/main/java/com/seven/zichen/snakegame/launcher/LaunchServer.java
@@ -0,0 +1,16 @@
+package com.seven.zichen.snakegame.launcher;
+
+import games_handler.GH_Manager;
+
+import java.io.IOException;
+
+public class LaunchServer {
+
+	public static void main(String[] args) throws IOException {
+		int nbJoueur = (args.length > 0 ? Integer.parseInt(args[0]) : 1);
+		System.out.println("Server initializing...");
+		Thread GH=new Thread(new GH_Manager(5757, 5656, "Snakes Server", 2000, nbJoueur));
+		GH.start();
+	}
+
+}
